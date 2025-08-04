3e495588b8b3edc58c416213debb74db9719b869
Sulfur dioxide <165257478+so-2@users.noreply.github.com>
2025-08-05 00:23:47 +0400

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+from typing import Optional
+
+import click
+from maclog.log import get_logger  # still needed for deps
+from pymobiledevice3.cli.cli_common import LockdownCommand, user_requested_colored_output
+from pymobiledevice3.lockdown import LockdownClient
+from pymobiledevice3.services.os_trace import OsTraceService
+
+FORMAT = 'CFPreference[{domain}][{user}][{key}] = {value}  # Process: {procname}'
+
+NO_VALUE_PREFIX = 'found no value '
+HAS_VALUE_PREFIX = 'looked up value '
+FOR_KEY_PREFIX = 'for key '
+FOR_KEY_SUFFIX = 'in CFPref'
+DOMAIN_PREFIX = '> (Domain: '
+USER_PREFIX = ', User: '
+
+DEFAULT_USER = 'kCFPreferencesAnyUser'
+BLOCKED_DOMAINS = {'com.apple.bluetooth', 'com.apple.MobileAsset', 'com.apple.mobilecal', 'com.apple.momentsd','com.apple.AppleMediaServices', 'com.apple.coremedia', 'com.apple.GEO', 'com.apple.locationd', 'com.apple.MobileBluetooth.debug', 'com.apple.coreimage', 'com.apple.mobilesafari', 'com.apple.powerlogd'}
+
+PREFS = {}
+
+def print_entry(message: str, filename: str, subsystem: Optional[str] = None, category: Optional[str] = None,
+                uni: bool = False, color: bool = False, undefined: bool = False) -> None:
+    if subsystem != 'com.apple.defaults' or category != 'User Defaults':
+        return
+
+    if 'cfprefs' not in message.lower():
+        return
+
+    if not message.startswith(HAS_VALUE_PREFIX) and not message.startswith(NO_VALUE_PREFIX):
+        return
+
+    user = DEFAULT_USER
+    value = None
+
+    key = message.split(FOR_KEY_PREFIX, 1)[1].split(FOR_KEY_SUFFIX, 1)[0].strip()
+    domain = message.rsplit(DOMAIN_PREFIX, 1)[1].split(',', 1)[0].strip()
+
+    if domain in BLOCKED_DOMAINS:
+        return  # skip blocked domains
+
+    procname = filename
+    has_value = False
+
+    if USER_PREFIX in message:
+        user = message.rsplit(USER_PREFIX, 1)[1].split(',', 1)[0].strip()
+
+    if not user:
+        user = DEFAULT_USER
+
+    if message.startswith(HAS_VALUE_PREFIX):
+        value = message.split(HAS_VALUE_PREFIX, 1)[1].split(FOR_KEY_PREFIX, 1)[0]
+        has_value = True
+
+    if uni:
+        seen_key = (domain, user, key)
+        if seen_key in PREFS:
+            return
+        PREFS[seen_key] = True
+
+    if color:
+        domain = click.style(domain, fg='yellow')
+        user = click.style(user, fg='bright_green')
+        key = click.style(key, fg='green')
+        procname = click.style(procname, fg='magenta')
+
+        if not has_value:
+            value = click.style(value, fg='red')
+
+    if (not undefined) or (undefined and not has_value):
+        print(FORMAT.format(domain=domain, user=user, key=key, value=value, procname=procname))
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(cls=LockdownCommand)
+@click.option('--uni', is_flag=True, help='output only unique entries')
+@click.option('--undefined', is_flag=True, help='filter only non-existing keys')
+def mobile(service_provider: LockdownClient, uni, undefined):
+    """ Sniff on connected iOS device """
+    for entry in OsTraceService(service_provider).syslog():
+        if entry.label is None:
+            continue
+        print_entry(entry.message, entry.filename, entry.label.subsystem, entry.label.category,
+                    uni=uni, undefined=undefined, color=user_requested_colored_output())
+
+
+if __name__ == '__main__':
+    cli()
